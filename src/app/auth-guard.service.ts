dcdd4ac8fc274c8db3af42b680595ab36171adb7
Lopes, Andre <andre.lopes@exxonmobil.com>
2017-09-12 16:24:56 -0300

    finished routing session

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from '@angular/router';
+import { Observable } from "rxjs/Observable";
+import { Injectable } from '@angular/core';
+import { AuthService } from "app/auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild {
+  canActivateChild(childRoute: ActivatedRouteSnapshot,
+                  state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate(childRoute, state);
+  }
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuthenticated()
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+          } else {
+            this.router.navigate(['/']);
+          }
+        }
+      );
+  }
+
+}
